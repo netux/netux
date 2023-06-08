06750f6786aa1b1e4429e06ad6a23d979412e15f
Martín (Netux) Rodríguez <send.email@netux.site>
2023-06-07 23:37:42 -0300

    feat(test/update): ensure working directory is script directory

--- test/update.sh
+++ test/update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+ROOT_DIR=$(dirname "$0")
+pushd $ROOT_DIR > /dev/null
+
 rm -r ./node_modules/netux # just in case
 npm pack ../
 PACKED_FILE_NAME=$(find netux-*.tgz)
 mv $PACKED_FILE_NAME netux-latest.tgz
 npm install file:./netux-latest.tgz
+
+popd
